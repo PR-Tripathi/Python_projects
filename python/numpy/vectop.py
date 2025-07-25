968d8a452150fde33fee04eefc9680f7771e61c2
PR-Tripathi <pravishtripathi7@gmail.com>
2025-07-26 00:00:27 +0530

    second chapter numpy

---
+++ python/numpy/vectop.py
@@ -0,0 +1,23 @@
+### Numpy Vectorized Operation
+import numpy as np
+
+arr1= np.array([1,2,3,4,5])
+arr2=np.array([10,20,30,40,50])
+
+### Element Wise addition
+print("Addition:", arr1+arr2)
+
+## Element Wise Substraction
+print("Substraction:", arr1-arr2)
+
+##Universal Function
+arr=np.array([2,3,4,5,6])
+
+##Sqaure root
+print(np.sqrt(arr))
+
+#Sine
+print(np.sin(arr))
+
+##Natural log
+print(np.log(arr))
