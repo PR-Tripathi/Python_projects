1008c405cde175daac2ee06c00551aef1a317d56
PR-Tripathi <pravishtripathi7@gmail.com>
2025-07-25 09:58:34 +0530

    new chnages

--- python/joiningpaths.py
+++ python/joiningpaths.py
@@ -1,7 +1,13 @@
 ## Joining Paths
 import os
 
-dir_name="folder"
-file_name="file.txt"
-full_path = os.path.join(os.getcwd(),dir_name,file_name)
-print(full_path)
+# dir_name="folder"
+# file_name="file.txt"
+# full_path = os.path.join(os.getcwd(),dir_name,file_name)
+# print(full_path) 
+
+path='example.txt'
+if os.path.exists(path):
+    print(f"The Path'{path}' exists")
+else:
+    print(f"the path '{path} dont exist'")
